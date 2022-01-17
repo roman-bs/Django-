7d09065a0a9669ecdcbc7a955ea23f229083f222
roman-bs <375445927023@mail.ru>
2022-01-17 22:04:06 +0300

    add classwork

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-   list_display = ("author","title", "slug", "created_at")
-   fields = ("author","title", "image", "slug", "text", "created_at")
+   list_display = ("author", "title", "slug", "created_at")
+   fields = ("author", "title", "image", "slug", "text", "created_at")
    readonly_fields = ("created_at",)
    search_fields = ("title", "slug", "text")
