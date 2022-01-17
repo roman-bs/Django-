dce8fd46b6f88a564c15e6dcc8332a000e9ac1cf
roman-bs <375445927023@mail.ru>
2022-01-17 18:50:13 +0300

    checkpoint

---
+++ blog/homework/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def homework_index(request):
+   value = request.GET.get("name")
+   logger.info(value)
+   return HttpResponse("Python the best")
