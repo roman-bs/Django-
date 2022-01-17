c6545c39be04478c662f4b2f3656b736cadbe246
roman-bs <375445927023@mail.ru>
2022-01-17 22:02:14 +0300

    add classwork

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+   list_display = ("title", "slug", "created_at")
+   fields = ("title", "image", "slug", "text", "created_at")
+   readonly_fields = ("created_at",)
+   search_fields = ("title", "slug", "text")
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, null=True)
+    slug = models.SlugField()
+    text = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+
